1aa3645595a25311aa9ad34af55d1b7ee6bf8338
Anthony M. Adinolfi <ama39@cornell.edu>
2019-03-12 14:33:41 -0400

    Latest theme updates from recent projects (Tabs and Expander, CKEditor fixes and enhancements, missing UL styles added, a few Triple-T's (Template Tag Tweaks), and most importantly: more cat photos)

--- js/cwd_utilities.js
+++ js/cwd_utilities.js
@@ -1,9 +1,11 @@
-/* CWD Utilities (ama39, last update: 1/29/19)
+/* CWD Utilities (ama39, last update: 2/25/19)
    - 1. Main Navigation (script support for dropdown menus and mobile)
-   - 2. Mobile Expander
-   - 3. Empty Sidebar Helper (clears whitespace from empty sidebar regions to allow use of the :empty pseudo class in CSS)
-   - 4. Read More Expander
-   - 5. Mobile Table Helper
+   - 2. Empty Sidebar Helper (clears whitespace from empty sidebar regions to allow use of the :empty pseudo class in CSS)
+   - 3. Mobile Table Helper (allows tables or other block elements to scroll horizontally on small devices, apply via .mobile-scroll class)
+   - 4. Expander (turns heading + div pairs into an expand/collapse system with nesting based on heading level)
+   - 5. Mobile Expander (similar to the standard expander, but intended to create single heading + div pairs that are only active at sub-tablet sizes (used, for example, by section navigation))
+   - 6. Read More Expander (shortens a block of text to an excerpt (if above a certain character count), and appends a "read more/close" toggle to reveal the rest, apply via .readmore-expander class)
+   - 7. Content Tabs (turns an ordered or unordered list into a set of slides with tabbed navigation) -- e.g., <ul class="content-tabs">
    ------------------------------------------------------------------------- */
 
 var mobile_breakpoint = 991; // viewport pixel width at which mobile nav appears (should match the media query in the project's css)
@@ -77,14 +79,14 @@ var msie = document.documentMode;
 	$('.dropdown-menu li.parent').parent().removeClass('menu').addClass('links list-menu');
 	$('.dropdown-menu li.parent > a').wrapInner('<span></span>').append('<span class="fa fa-caret-down"></span>'); // wrap text in a span and add dropdown caret icons
 	$('.dropdown-menu li.parent li.parent > a .fa').removeClass('fa-caret-down').addClass('fa-caret-right'); // change sub-dropdown caret icons
-	$('.dropdown-menu li.parent > ul').each(function(){
+	$('.dropdown-menu li.parent > ul').each(function() {
 		$(this).removeClass('menu').addClass('list-menu links vertical children');
 		if ( !$('body').hasClass('mobile') ) {
 			$(this).css('min-width',$(this).parent('li').width()+'px' ); // smart min-width to prevent dropdown from being narrower than its parent
 		}
 	});
 	$('.dropdown-menu li.parent li.parent > ul').removeAttr('style'); // reset min-width to allow smaller submenus
-	$('.dropdown-menu li.parent').hover(function(){
+	$('.dropdown-menu li.parent').hover(function() {
 		if ( !$('body').hasClass('mobile') ) {
 			// horizontal edge-detection
 			var submenu_offset = $(this).children('ul').offset();
@@ -148,16 +150,88 @@ var msie = document.documentMode;
 	$('#mobile-nav-dimmer').click(function(e) {
 		$('#mobile-close').trigger('click');
 	});
-
-
-
-
-
-
-
-
-
-	// 2. Mobile Expander -----------------------------------------------------
+	
+	
+	// 2. Empty Sidebar Helper ------------------------------------------------
+	$('.secondary').each(function() {
+		if (msie != 8 && msie != 7) {
+			if ( !$(this).html().trim() ) {
+				$(this).empty().addClass('empty');
+			}
+		}
+	});
+	
+	
+	// 3. Mobile Table Helper -------------------------------------------------
+	$('.mobile-scroll').each(function() {
+			$(this).wrap('<div class="table-scroller" />');
+			if ( $(this).hasClass('large') ) {
+				$(this).parent().addClass('large');
+			}
+	});
+	$('.table-scroller').append('<div class="table-fader" />').bind('scroll touchmove', function() {
+		$(this).find('.table-fader').remove(); // hide fader DIV on user interaction
+	});
+	
+	
+	// 4. Expander ------------------------------------------------------------
+	$('.expander').addClass('scripted').find('h2, h3, h4, h5, h6').each(function(i) {
+		if ($(this).next('div').length > 0) {
+			//$(this).next('div').addClass('expander-div aria-target').attr('tabindex','0');
+			$(this).addClass('sans expander-heading').attr('tabindex','0').attr('aria-expanded','false').prepend('<span class="fa fa-plus-square-o" aria-hidden="true"></span>');
+			$(this).click(function(e) {
+				$(this).toggleClass('open');
+				if ($(this).hasClass('open')) {
+					$(this).attr('aria-expanded','true');
+				}
+				else {
+					$(this).attr('aria-expanded','false');
+				}
+			});
+		}
+	});
+	$('.expander').each(function() {
+		if ($(this).find('.expander-heading').length > 2) {
+			var all_expanded = false;
+			$(this).prepend('<button class="expand-all">Expand all</button>');
+			$(this).children('.expand-all').click(function(e) {
+				e.preventDefault();
+				if (!all_expanded) {
+					$(this).parent().find('.expander-heading').addClass('open');
+					$(this).addClass('open');
+					all_expanded = true;
+					$(this).text('Close all');
+				}
+				else {
+					$(this).parent().find('.expander-heading').removeClass('open');
+					$(this).removeClass('open');
+					all_expanded = false;
+					$(this).text('Expand all');
+				}
+			});
+		}
+	});
+	$('.expander-heading').each(function() {
+		
+		/*
+		var this_heading = $(this);
+		$(this).next('div').focus(function() {
+			$(this_heading).addClass('open');
+		});
+		$(this).next('div').find('*').focus(function() {
+			$(this_heading).addClass('open');
+		});
+		*/
+		
+		$(this).keydown(function(e) {
+			if (e.keyCode == 13 || e.keyCode == 32) { // enter or space key
+				e.preventDefault();
+				$(this).trigger('click');
+			}
+		});
+	});
+	
+	// 5. Mobile Expander -----------------------------------------------------
 	//$('.drupal #sidebar-top nav, nav.nav-body').addClass('mobile-expander').prepend('<h1 class="sans nav-heading">In this section<span class="punc">:</span></h1>');
 	//$('.drupal.page-search-site #sidebar-top nav .nav-heading').addClass('hidden').html('Filter results<span class="punc">:</span>');
 	$('.mobile-expander').each(function() {
@@ -195,17 +269,7 @@ var msie = document.documentMode;
 		//}
 	//});
 
-	// 3. Empty Sidebar Helper ------------------------------------------------
-	$('.secondary').each(function() {
-		if (msie != 8 && msie != 7) {
-			if ( !$(this).html().trim() ) {
-				$(this).empty();
-			}
-		}
-	});
-
-
-	// 4. Read More Expander --------------------------------------------------
+	// 6. Read More Expander --------------------------------------------------
 	var excerpt_length = 310;
 	var button_label_expand = 'Read More';
 	var button_label_collapse = 'Close';
@@ -243,25 +307,134 @@ var msie = document.documentMode;
 			$(this).parents('.readmore-expander').first().next('.readmore-excerpt-container').find('.readmore-expander-button').trigger('click');
 		}
 	});
-
-
-
-	// 5. Mobile Table Helper -------------------------------------------------
-	$('.mobile-scroll').each(function() {
-			$(this).wrap('<div class="table-scroller" />');
-			if ( $(this).hasClass('large') ) {
-				$(this).parent().addClass('large');
+	
+	
+	// 7. Content Tabs --------------------------------------------------------
+	$('.content-tabs').each(function() {
+		
+		var aria_mode = false;
+		var nav_tag = 'nav';
+		var tab_tag = 'a';
+		
+		// prepare class options to share with tab navigation 
+		var tab_classes = 'tabs-nav';
+		if ( $(this).hasClass('tabs-classic') ) {
+			tab_classes += ' tabs-classic';
+		}
+		if ( $(this).hasClass('tabs-mobile-expand') ) {
+			tab_classes += ' tabs-mobile-expand';
+		}
+		
+		if ( $(this).hasClass('tabs-numbered') ) {
+			tab_classes += ' tabs-numbered';
+		}
+		if ( $(this).hasClass('tabs-numbers-only') ) {
+			tab_classes += ' tabs-numbers-only';
+		}
+		if ( $(this).hasClass('tabs-aria') ) {
+			tab_classes += ' tabs-aria';
+			aria_mode = true;
+			nav_tag = 'div';
+			tab_tag = 'button';
+		}
+		if ( $(this).hasClass('tabs-mobile-accordion') ) {
+			tab_classes += ' tabs-mobile-accordion'; // NYI
+		}
+		if ( $(this).hasClass('tabs-reserve-height') ) {
+			tab_classes += ' tabs-reserve-height'; // NYI
+		}
+		
+		// generate navigation
+		$(this).before('<'+nav_tag+' aria-label="Choose a Tab" class="'+tab_classes+'"></'+nav_tag+'>').addClass('scripted').children('li').each(function(i){
+			var tab_title = $(this).find('h1,h2,h3,h4,h5,h6').first().text();
+			var tab_id = 'tab-' + Math.floor(Math.random()*26) + Date.now(); // generate unique ID to allow links to target their tabs for better screen reader accessibility
+			var tab_number = '';
+			var tab_labelbefore = '';
+			var tab_labelafter = '';
+			if ( $(this).parent().hasClass('tabs-numbers-only') ) {
+				tab_number = (i+1) + ' ';
+				tab_labelbefore = '<span class="hidden">(';
+				tab_labelafter = ')</span>';
 			}
+			else if ( $(this).parent().hasClass('tabs-numbered') ) {
+				tab_number = (i+1) + '. ';
+			}
+			if (aria_mode) {
+				$(this).parent().prev(nav_tag).attr('role', 'tablist').append('<'+tab_tag+' role="tab" aria-selected="false" aria-controls="'+tab_id+'">'+ tab_number + tab_labelbefore + tab_title + tab_labelafter + '</'+tab_tag+'>');
+				$(this).attr('id',tab_id).attr('role','tabpanel').wrapInner('<div class="tab-inner"></div>').attr('tabindex','0').attr('aria-label', tab_title).addClass('aria-target').hide();
+			}
+			else {
+				$(this).parent().prev(nav_tag).append('<a href="#'+tab_id+'">'+ tab_number + tab_labelbefore + tab_title + tab_labelafter + '</a>');
+				$(this).attr('id',tab_id).wrapInner('<div class="tab-inner"></div>').attr('tabindex','-1').attr('aria-label','Tab: ' + tab_title).addClass('aria-target').hide();
+			}
+		});
+		$(this).children('li').first().show().addClass('active');
+		
+		// tab navigation button events
+		$(this).prev(nav_tag).each(function() {
+			var tabs = $(this).next('.content-tabs');
+			$(this).children(tab_tag).first().addClass('active').attr('aria-selected','true');
+			$(this).children(tab_tag).click(function(e) {
+				e.preventDefault();
+				$(tabs).children('li').removeClass('active').hide();
+				$(tabs).children('li').eq( $(this).index() ).show().addClass('active');
+				$(tabs).prev(nav_tag).find(tab_tag).removeClass('active').attr('aria-selected','false');
+				$(this).addClass('active').attr('aria-selected','true');
+				if (!aria_mode) {
+					$($(this).attr('href')).focus();
+				}
+			});
+			
+			// arrow key navigation for ARIA tabs
+			if (aria_mode) {
+				var tab_count = $(this).children(tab_tag).length;
+				$(this).children(tab_tag).each(function() {
+					var prev_tab = $(this).index() - 1;
+					var next_tab = $(this).index() + 1;
+					if (prev_tab < 0) {
+						prev_tab = tab_count - 1;
+					}
+					if (next_tab > tab_count - 1) {
+						next_tab = 0;
+					}
+					$(this).keydown(function(e) {
+						if (e.keyCode == 37) { // left arrow key
+							e.preventDefault();
+							$(this).siblings().addBack().eq(prev_tab).trigger('click').focus();
+						}
+						else if (e.keyCode == 39) { // right arrow key
+							e.preventDefault();
+							$(this).siblings().addBack().eq(next_tab).trigger('click').focus();
+						}
+					});
+				});
+			}
+		});
 	});
-	$('.table-scroller').append('<div class="table-fader" />').bind('scroll touchmove', function() {
-		$(this).find('.table-fader').remove(); // hide fader DIV on user interaction
-	});
 
 
+/*
+<div class="tabs">
+	<div role="tablist" aria-label="Entertainment">
+	  <button role="tab" aria-selected="true" aria-controls="nils-tab" id="nils">Nils Frahm</button>
+	  <button role="tab" aria-selected="false" aria-controls="agnes-tab" id="agnes" tabindex="-1">Agnes Obel</button>
+	  <button role="tab" aria-selected="false" aria-controls="complexcomplex" id="complex" tabindex="-1" data-deletable="">Joke</button>
+	</div>
 
+	<div tabindex="0" role="tabpanel" id="nils-tab" aria-labelledby="nils">
+	  <p>Nils Frahm is a German musician, composer and record producer based in Berlin. He is known for combining classical and electronic music and for an unconventional approach to the piano in which he mixes a grand piano, upright piano, Roland Juno-60, Rhodes piano, drum machine, and Moog Taurus.</p>
+	</div>
 
+	<div tabindex="0" role="tabpanel" id="agnes-tab" aria-labelledby="agnes" hidden="">
+	  <p>Agnes Caroline Thaarup Obel is a Danish singer/songwriter. Her first album, Philharmonics, was released by PIAS Recordings on 4 October 2010 in Europe. Philharmonics was certified gold in June 2011 by the Belgian Entertainment Association (BEA) for sales of 10,000 Copies.</p>
+	</div>
 
-
+	<div tabindex="0" role="tabpanel" id="complexcomplex" aria-labelledby="complex" hidden="">
+	  <p>Fear of complicated buildings:</p>
+	  <p>A complex complex complex.</p>
+	</div>
+</div>
+*/
 
 
 
